981c412b8e5018474a38b8d11ebd145445d51fe2
王立彦 <wangliyan@uni-ubi.com>
2020-03-31 15:49:53 +0800

    looper实现界面卡顿检测

---
+++ app/src/main/java/com/wly/handlerdemo/MainActivity.kt
@@ -0,0 +1,65 @@
+package com.wly.handlerdemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.HandlerThread
+import android.os.Looper
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    private val checkHandler = CheckHandler()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        check()
+        btn_check.setOnClickListener {
+            Thread.sleep(2000)
+        }
+    }
+
+    private fun check(){
+        Looper.getMainLooper().setMessageLogging {
+            if (it.startsWith(">>>>> Dispatching to")){
+                checkHandler.onStart()
+            }else if (it.startsWith("<<<<< Finished to")){
+                checkHandler.onEnd()
+            }
+        }
+    }
+
+    class CheckHandler {
+        private val mHandlerThread = HandlerThread("卡顿检测")
+
+        private var mHandler : Handler
+
+        constructor(){
+            mHandlerThread.start()
+            mHandler = Handler(mHandlerThread.looper)
+        }
+
+        private val runnable = Runnable {
+            log()
+        }
+
+        fun onStart(){
+            mHandler.postDelayed(runnable , 1000)
+        }
+
+        fun onEnd(){
+            mHandler.removeCallbacksAndMessages(null)
+        }
+
+        fun log() {
+            val sb = StringBuilder()
+            val stackTrace = Looper.getMainLooper().thread.stackTrace
+            stackTrace.forEach {
+                sb.append("${it}\n")
+            }
+            Log.w("TAG", sb.toString())
+        }
+    }
+}
